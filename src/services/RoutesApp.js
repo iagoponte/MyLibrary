3cc64574b40870498b065833ccdf2143addf93c0
Guilherme <guiilhermemacielcarneiro@gmail.com>
2025-01-28 21:14:08 -0300

    Footer-Estilo-Alteração

--- src/services/RoutesApp.js
+++ src/services/RoutesApp.js
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 import Home from '../pages/Home';
 import Detalhes from '../pages/Detalhes';
 import Cadastro from '../pages/Cadastro';
 
 import Header from '../components/Header';
-
+import Footer from '../components/Footer';
 export default function RoutesApp() {
     return (
         <>
@@ -16,7 +15,9 @@ export default function RoutesApp() {
                     <Route path='/' element={<Home />} />
                     <Route path='/detalhes' element={<Detalhes />} />
                     <Route path='/Cadastrar' element={<Cadastro />} />
+        
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </>
     );
