e4faeb659f96ed71bc2656dceb85871dfb455bf1
=Mateus Helcias <=matheushelciascabr1@gmail.com>
2025-01-30 21:22:39 -0300

    atualizacao cadastro

---
+++ src/components/buscaDados/index.js
@@ -0,0 +1,22 @@
+// export default function BuscaDados(){
+
+//     return(
+//         <div>
+
+//         </div>
+//     );
+// }
+
+
+export const buscaDados = ()=> {
+    let bd = localStorage.getItem("books");
+
+    
+    
+    if(bd === null){
+        bd = [];
+    }else{
+        bd = JSON.parse(bd);
+    }
+    return bd;
+}
