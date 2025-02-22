aa21272d15e437ee16f73dc8ed279ade00f63c3f
iagop <iago.ponte2@gmail.com>
2025-02-22 16:58:40 -0300

    client-Models-Controls

--- Backend/src/controllers/cliente.js
+++ Backend/src/controllers/cliente.js
@@ -1,4 +1,4 @@
-const { getClientesQuery } = require("../models/clienteModel");
+const { getClientesQuery, searchClientesByNomeEmailQuery } = require("../models/clienteModel");
 
 const getClientes = async (req, res) => {
   try {
@@ -17,4 +17,40 @@ const getClientes = async (req, res) => {
   }
 };
 
-module.exports = { getClientes };
+const createCliente = async (req, res) => {
+  const { nome_usuario, email, senha_hash } = req.body;
+  if (!nome_usuario || !email || !senha_hash) {
+    return handle400Error(req, res, "verifique as info");
+  }
+  const checarClienteExistente = await searchClientesByNomeEmailQuery(
+    nome_usuario,
+    email
+  );
+  console.log(checarClienteExistente);
+
+  if (checarClienteExistente !== null) {
+    return handle400Error(
+      req,
+      res,
+      "já existe um cliente com esse título e autor"
+    );
+  }
+  try {
+    const query = await createClienteQuery(
+      nome_usuario,
+      
+    );
+    if (query == null) {
+      handle404Error(req, res, "Deu erro");
+    } else {
+      handle200(req, res, "Sucesso", query);
+    }
+  } catch (error) {
+    console.error(
+      "Sorry, something went wrong on our end. Please try again later.",
+      error
+    );
+  }
+};
+
+module.exports = { getClientes, createCliente };
--- Backend/src/models/clienteModel.js
+++ Backend/src/models/clienteModel.js
@@ -9,5 +9,26 @@ const getClientesQuery = async () => {
   return query;
 };
 
+const createClientQuery = async (nome_usuario, email, senha_hash) => {
+  try {
+    const query =
+      await connection`INSERT INTO clientes (nome_usuario, email, senha_hash) VALUES (${nome_usuario}, ${email}, ${senha_hash}) RETURNING *`;
+    return query[0];
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports = { getClientesQuery };
+const searchClientesByNomeEmailQuery = async (nome_usuario, email) => {
+  const query =
+    await connection`SELECT * FROM clientes l WHERE l.nome_usuario = ${nome_usuario} AND l.email = ${email}`;
+  if (query.length === 0) {
+    // console.log(query)
+    // console.error("Os livros não foi encontrado");
+    return null;
+  }
+  return query;
+};
+
+
+module.exports = { getClientesQuery, createClientQuery, searchClientesByNomeEmailQuery };
