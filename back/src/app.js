58dbd101f9b432b44f37ff46e8cc62df95ecf17f
iagop <iago.ponte2@gmail.com>
2025-03-06 14:41:29 -0300

    fix: cors in app.js

--- back/src/app.js
+++ back/src/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = require('./router');
 const cors = require('cors')
 
-app.use(cors());
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
